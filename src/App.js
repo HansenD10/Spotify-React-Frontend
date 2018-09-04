8b864376ed678f23b1daaeac571801aa11e84fc8
HansenD10 <Dylan.hansen10@gmail.com>
2018-09-04 18:43:25 -0500

    Adding api call to get user information.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import qs from 'query-string'
+import axios from 'axios'
+import NavBar from './Components/NavBar/NavBar'
 
 class App extends Component {
   constructor() {
@@ -18,15 +20,35 @@ class App extends Component {
       window.location.replace('http://localhost:8080/login')
   }
 
-  
+  getUserInfo = () => {
+    axios.get('https://api.spotify.com/v1/me', {
+      headers: {
+        'Authorization': 'Bearer ' + this.state.access_token
+      }
+    }).then(({data}) => {
+      this.setState({
+        user_data: {
+          name: data.display_name,
+          href: data.href,
+          image: data.images[0].url || null
+        }
+      })
+    })
+  }
+
+
   componentWillMount() {
     this.validateUser()
+  }
+
+  componentDidMount() {
+    this.getUserInfo()
   }
 
   render() {
     return (
       <div>
-        
+        <NavBar />
       </div>
     );
   }
