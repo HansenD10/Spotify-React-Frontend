18e502c5526c6f0e61a59248c9baa9ff897d827f
HansenD10 <Dylan.hansen10@gmail.com>
2018-09-04 19:10:29 -0500

    Adding NavBar component with styling.

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './NavBar.css'
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div className="nav-wrapper">
+        <p>Spotify React</p>
+        
+      </div>
+    )
+  }
+}
